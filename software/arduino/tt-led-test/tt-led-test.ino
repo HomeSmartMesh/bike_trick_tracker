f40d59c4d49e286b980fc0c801730e6dbc4fc4d4
Unknown <mirko.haeberlin@gmail.com>
2022-08-20 15:29:05 +0200

    add testpoints + test programms

--- software/arduino/tt-led-test/tt-led-test.ino
+++ software/arduino/tt-led-test/tt-led-test.ino
@@ -9,9 +9,7 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  if (Serial.available()) {
-    Serial.write("hallo");
-  }
+  Serial.write("hallo");
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
